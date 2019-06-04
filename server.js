3dcb7f66bef706907484566c55959ef138225953
esperienzia <32093221+esperienzia@users.noreply.github.com>
2019-06-04 16:54:27 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const moment = require('moment');
+
+const APP = express();
+APP.get('/', function (req, res) {
+  res.send(`Express Server online at - ${moment().format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+});
+
+APP.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
