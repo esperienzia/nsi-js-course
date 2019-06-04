db23315991a6418fd518c2deb86eb0454496c54e
d.mellonch <d.melloncelli@nsi.it>
2019-06-04 17:02:04 +0200

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 const APP = express();
 APP.get('/', function (req, res) {
-  res.send(`Express Server online at - ${moment().format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+  res.send(`Express Server online - ${moment().format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
 });
 
 APP.listen(3000, function () {
