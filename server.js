a9d6d26f9b756b648f426903808d01f1c82351d5
d.mellonch <d.melloncelli@nsi.it>
2019-06-07 16:36:02 +0200

    cose (2)

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const moment = require('moment');
-const fs = require('fs');
+const {promisify: _p} = require('util');
+
+
 const path = require('path');
 const bodyParser = require('body-parser')
 const APP = module.exports = express();
-
+const _generateUuid = require('uuid/v1');
 if (require.main === module) {
   const PORT = Number(process.env.APP_PORT || 3000);
   APP.listen(PORT, function () {
@@ -14,43 +16,58 @@ if (require.main === module) {
   });
 }
 APP.use(express.json());
+APP.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 APP.get('/', function (req, res) {
-
   res.send(`Express Server online - ${moment().format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
 });
 
+function buildPath(id, noExtension) {
+  const NOTES_FOLDER_NAME = 'notes';
+  const PATH_ARRAY = [__dirname, NOTES_FOLDER_NAME];
+  if (id) {
+    PATH_ARRAY.push(`${id}${noExtension ? '' : '.txt'}`);
+  }
+  return PATH_ARRAY.join(path.sep);
+}
+async function _getNotes(req, res) {
+  const fs = require('fs');
+  const FILES_NAME = await _p(fs.readdir)(buildPath(null));
+  const FILES_DATA = await Promise.all(FILES_NAME.map(fileName => {
+    return _p(fs.readFile)(buildPath(fileName, true), 'utf8');
+  }));
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(FILES_DATA.map((data, idx) => ({id: FILES_NAME[idx], note: data}))));
+}
 
 APP.route('/notes')
-   .get(function (req, res) {
-     res.send('Get all books')
+   .get((req, res) => {
+     _getNotes(req, res).catch((err) => {
+       res.end(JSON.stringify(err));
+     });
    })
    .post(function (req, res) {
-     const ID = moment().unix();
+     const fs = require('fs');
+     const ID = _generateUuid();
      if (!req.body.note) {
        return res.status(400).send('Field note is required');
      }
-     fs.writeFile(buildPath(ID), req.body.note,'utf8' ,(err, file) => {
+     fs.writeFile(buildPath(ID), req.body.note, 'utf8', (err) => {
        if (err) {
          return res.status(500).send('Something broke!');
        }
        res.statusCode = 200;
        res.setHeader('Content-Type', 'application/json');
-       res.send(JSON.stringify({id: ID, note: req.body}));
+       res.send(JSON.stringify({id: ID, note: req.body.note}));
      });
    })
    .delete(function (req, res) {
      res.send('Delete all the book')
    });
-
-function buildPath(id) {
-  const NOTES_FOLDER_NAME = 'notes';
-  const PATH_ARRAY = [__dirname, NOTES_FOLDER_NAME];
-  if (id) {
-    PATH_ARRAY.push(id + '.txt');
-  }
-  return PATH_ARRAY.join(path.sep);
-}
 
 APP.route('/notes/:id')
    .get(function (req, res) {
